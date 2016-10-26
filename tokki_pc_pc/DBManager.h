cbb4836e8e5756fac73341c7bd474e7a44835d34
NOorYes <1313susu@gmail.com>
2016-10-26 22:37:02 +0900

    firerrow

--- tokki_pc_pc/DBManager.h
+++ tokki_pc_pc/DBManager.h
@@ -9,7 +9,7 @@ public:
 	DBManager();
 	~DBManager();
 
-	bool Login(const string& ID, const string& password);
+	bool Login(const string& ID, const string& password)
 	bool Register(Member);
 	// Member 는 PC managerment 클라이언트에서 받아 옵니다.(?)
 
--- tokki_pc_pc/StatusUpdater.h
+++ tokki_pc_pc/StatusUpdater.h
@@ -1,6 +1,7 @@
 #pragma once
 
-
+// 개별 PC의 상태를 계속 업그레이드 하는 클래스,
+// 매초마다 호출된다.
 class StatusUpdater
 {
 public:
@@ -9,6 +10,13 @@ public:
 
 private:
 	enum current_pc_status
-		// 열거형으로, 
-	
+	{	
+		is_active = true,
+		is_power_on = true
+		
+		// 여기에는 무슨 정보가 있는지?
+		// PC의 상태니까 PC_ON PC_OFF 같은것?
+	};
+		// pc의 정보를 담고 있는 열거형
+	UpdaterThredad // ? 이
 };
